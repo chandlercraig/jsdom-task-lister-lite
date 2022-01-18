032c792166244c40a72b064c1786c1c5c3f91caa
Chandler Craig <hellochandlercraig@gmail.com>
2022-01-17 20:27:08 -0800

    commit tree 0c3da4898ba59363a44c24905145bc974560533c

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+document.getElementById("create-task-form").addEventListener("submit", function(e) {
+    const txt = document.getElementById("new-task-description").value;
+    const lstSec = document.getElementById("tasks");
+    const nwLstEl = document.createElement('li');
+    const tskLn = lstSec.appendChild(nwLstEl);
+
+    tskLn.innerHTML = txt;
+
+    e.preventDefault();
+  });
 });
